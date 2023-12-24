f8a143c18cbd8faa52581c919a12aa5a8c88326b
AntrikshSaini13 <antrikshsaini13@gmail.com>
2023-12-25 00:51:36 +0530

    c-lang

--- 18-Assignment Array/Question-1.c
+++ 18-Assignment Array/Question-1.c
@@ -1,28 +1,22 @@
-#include<stdio.h>
-void sum_array(int);
-int main(){
-    int size_of_array;
-    printf("Who many value instert in array between 1-100 \n");
-    scanf("%d",&size_of_array);
+// Write a program to calculate the sum of number stored in an Array of size 10. Teke
+// array vlaue from the user ?
 
-    // call here sum function
-    sum_array(size_of_array);
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+int sumOfArray(int array[],int size);
+int sumOfArray(int array[],int size){
+    int sum, i;
+    for(i = 0, sum = 0; i < size;i++)
+        sum += array[i];
+        return sum;
 }
-// sum function of array
-void sum_array(int size_of_array){
-    int a[100], index_of_array,sum_of_array_elements=0;
-    printf("Enter the array elements\n ");
-    for(index_of_array = 0; index_of_array < size_of_array; index_of_array++){
-        scanf("%d",&a[index_of_array]);
-    }
-    for(index_of_array = 0; index_of_array < size_of_array; index_of_array++){
-        printf("Array[%d] = %d\n",index_of_array,a[index_of_array]);
-        sum_of_array_elements+=a[index_of_array];
-    }
-
-    // for(index_of_array = 0; index_of_array < size_of_array;index_of_array++){
-    //     sum_of_array_elements+=a[index_of_array];
-    // }
-    printf("sum of array is %d",sum_of_array_elements);
+void main(){
+    int i,size,array[100];
+    printf("Enter the size of array");
+    scanf("%d",&size);
+    puts("Enter the array elements");
+    for(i=0;i<size;i++)
+        scanf("%d",&array[i]);
+    printf("Sum of array ->  %d",sumOfArray(array,size));
+    
 }
