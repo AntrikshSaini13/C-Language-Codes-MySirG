8fb36d9b72b0c577da9fdc141eb47206a7a244b1
AntrikshSaini13 <antrikshsaini13@gmail.com>
2024-07-23 15:28:01 +0530

    push code

--- 1-Assignment printf and scanf/Question-1.c
+++ 1-Assignment printf and scanf/Question-1.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
+#include<conio.h>
 
 void main(){
-    printf("Hello World");
+   int id, sal;
+   char ch1,ch2;
+   printf("Enter the char : ");
+     
+   scanf("%s",ch1);
+   
+   printf("Enter the char : ");
+   getchar();
+   scanf("%s",ch2);
+
+
+  printf("%s %s",ch1,ch2);
+
+//    printf("Enter the char : ");
+//    scanf("%c",&ch);
+
+//    printf("%c",ch);
+  
+
 }
